9ddd27381866a55a2872b0141bcffc223c179937
devg1120 <devg1120@gmail.com>
2022-08-11 18:01:36 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+//import "flex-splitter-directive"
+//import "flex-splitter-directive/styles.min.css"
+
+import "./flex-splitter-directive/index.js"
+import "./flex-splitter-directive/styles.min.css"
+
